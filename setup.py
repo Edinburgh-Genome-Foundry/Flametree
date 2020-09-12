b06d8a3cb3ac393ac9dea3f29122dd75c819e279
Peter Vegh <pvegh@posteo.uk>
2020-09-12 19:14:58 +0100

    License text fix

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 import ez_setup
+
 ez_setup.use_setuptools()
 
 from setuptools import setup, find_packages
 
-exec(open('flametree/version.py').read()) # loads __version__
+exec(open("flametree/version.py").read())  # loads __version__
 
-setup(name='flametree',
-      version=__version__,
-      author='Zulko',
-      description='Python file and zip operations made easy',
-      url='https://github.com/Edinburgh-Genome-Foundry/Flametree',
-      long_description=open('pypi-readme.rst').read(),
-      license='MIT',
-      keywords="file system, zip, archive, file, directory",
-      packages=find_packages(exclude='docs'))
+setup(
+    name="flametree",
+    version=__version__,
+    author="Zulko",
+    description="Python file and zip operations made easy",
+    url="https://github.com/Edinburgh-Genome-Foundry/Flametree",
+    long_description=open("pypi-readme.rst").read(),
+    license="MIT",
+    keywords="file system, zip, archive, file, directory",
+    packages=find_packages(exclude="docs"),
+)
